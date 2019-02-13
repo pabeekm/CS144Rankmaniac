3b8162dfc46d5c613924fc27c7477732281829e5
pabeekm <patriciabeekman@Patricias-MacBook-Pro.local>
2019-02-12 22:24:13 -0800

    Add iteration counter

--- data/process_map.py
+++ data/process_map.py
@@ -6,5 +6,21 @@ import sys
 # This program simply represents the identity function.
 #
 
+i = 0
+i_prefix = "$"
+
 for line in sys.stdin:
-    sys.stdout.write("key\t" + line)
+    # Get the iteration number if we see it
+    if line.startswith(i_prefix):
+        i = int(line.split("\t")[1])
+        continue
+
+    # Grab the node_id 
+    node_id = int((line.split(":")[0]))
+
+    # Now just use a generic key so the reducer can be in charge of sorting all output and
+    # checking whether or not prev_ranks have changed
+    sys.stdout.write("key:" + str(node_id) + "," + line.split(":")[1])
+
+# Pass along the iteration number
+sys.stdout.write("$\t%d\n" % (i))
